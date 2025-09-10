2f6b4e8b1dbf8812bd33f213fc00e254ad899b5c
christian.slapak <christian.slapak@ort.edu.ar>
2025-09-10 20:09:02 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp-2025C2-Mie-Primero/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2025C2_Mie_Primero
+{
+    internal class Persona
+    {
+        public int dni { get; set; }
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+
+        public string MostrarDatos()
+        {
+            string mensaje = "";
+            mensaje = "La persona es: " + dni + " " + apellido + " " + nombre;
+            return mensaje;
+        }
+    }
+}
---
+++ ConsoleApp-2025C2-Mie-Primero/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp_2025C2_Mie_Primero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Persona p = new Persona();
+            p.dni = 34567890;
+            p.apellido = "De Paul";
+            p.nombre = "Rodigo";
+
+            Console.WriteLine(p.MostrarDatos());
+
+        }
+    }
+}
