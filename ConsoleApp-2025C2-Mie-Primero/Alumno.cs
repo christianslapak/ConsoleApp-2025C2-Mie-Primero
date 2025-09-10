2d1eb4f10a9997780905e3c47194ad8267db9975
christian.slapak <christian.slapak@ort.edu.ar>
2025-09-10 20:28:25 -0300

    Correccion Print Alumno

--- ConsoleApp-2025C2-Mie-Primero/Alumno.cs
+++ ConsoleApp-2025C2-Mie-Primero/Alumno.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleApp_2025C2_Mie_Primero
 {
-    internal class Alumno : Persona
+    internal class Alumno:Persona
     {
         public int legajo { get; set; }
         public string materia { get; set; }
+    }
+
+    public string MostrarDatos()
+    {
+        string mensaje = "";
+        mensaje = base.MostrarDatos() + " " + legajo + " " + materia;
+        return mensaje;
     }
 }
